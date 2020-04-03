c916f42b5a45a855184c9907bf15c080731bcd77
sungwook su <su@pocketgems.com>
2020-04-02 20:39:33 -0700

    add missing preview script

---
+++ Assets/Scripts/SimplePreview.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+namespace ARKitStream
+{
+    public class SimplePreview : MonoBehaviour
+    {
+        [SerializeField] AROcclusionManager occlusionManager = null;
+        [SerializeField] Material material = null;
+
+        void Update()
+        {
+            var stencil = occlusionManager.humanStencilTexture;
+            var depth = occlusionManager.humanDepthTexture;
+
+            material.SetTexture("_textureStencil", stencil);
+            material.SetTexture("_textureDepth", depth);
+        }
+    }
+}
